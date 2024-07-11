a334e6cab482fac9bf2b6b3f6c75d23ca3a6ca52
hgb926 <hgb926@naver.com>
2024-07-11 11:41:35 +0900

    0711 route 리팩터링, 인증 페이지 컴포넌트

--- src/components/EventSkeleton.js
+++ src/components/EventSkeleton.js
@@ -2,29 +2,31 @@ import React from 'react';
 import styles from './EventSkeleton.module.scss';
 
 const EventSkeleton = ({ count }) => {
-  return (
-    <div className={styles.events}>
-      <div className={styles.list}>
-        {
-          Array.from(new Array(count)).map(
-            (_, index) => (
-              <div
-                className={styles.skeleton}
-                key={index}
-              >
-                <div className={styles.imageSkeleton} />
-                <div className={styles.contentSkeleton}>
-                  <div className={styles.titleSkeleton} />
-                  <div className={styles.dateSkeleton} />
-                </div>
-              </div>
-            )
-          )
-          
-        }
-      </div>
-    </div>
-  );
+    // count 값이 유효한지 확인, 음수이거나 유효하지 않으면 0으로 설정
+    const validCount = Number.isInteger(count) && count > 0 ? count : 0;
+
+    return (
+        <div className={styles.events}>
+            <div className={styles.list}>
+                {
+                    Array.from(new Array(validCount)).map(
+                        (_, index) => (
+                            <div
+                                className={styles.skeleton}
+                                key={index}
+                            >
+                                <div className={styles.imageSkeleton} />
+                                <div className={styles.contentSkeleton}>
+                                    <div className={styles.titleSkeleton} />
+                                    <div className={styles.dateSkeleton} />
+                                </div>
+                            </div>
+                        )
+                    )
+                }
+            </div>
+        </div>
+    );
 };
 
 EventSkeleton.displayName = 'EventSkeleton';
---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUpPage = () => {
+    return (
+        <h1>
+            SignUpPage!
+        </h1>
+    );
+};
+
+export default SignUpPage;
