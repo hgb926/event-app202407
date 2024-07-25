5f0bf88eb9fd2d8029d2704fdd70811825b77d39
hgb926 <hgb926@naver.com>
2024-07-25 11:37:29 +0900

    0725 자동배포 확인

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -1,5 +1,5 @@
 
-const LOCAL_PORT = 8585; // 백엔드 로컬 서버 포트번호
+const LOCAL_PORT = 8787; // 백엔드 로컬 서버 포트번호
 
 // 예를들어 지금 요청하는 브라우저의 host가 http://localhost:3000 이라면
 // hostname은 localhost만 리턴
@@ -10,8 +10,8 @@ let backendHostName;
 
 if (clientHostName === 'localhost') {
   backendHostName = 'http://localhost:' + LOCAL_PORT;
-} else if (clientHostName === 'www.bananagrape.co.kr') {
-  backendHostName = 'https://api.myapi.com';
+} else if (clientHostName === 'my-event-app980926.s3-website.ap-northeast-2.amazonaws.com') {
+  backendHostName = 'http://43.203.58.236:8787';
 }
 
 const API_BASE_URL = backendHostName;
